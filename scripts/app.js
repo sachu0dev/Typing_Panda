97e07753e619f60d99a0edb2853a3e77ef5e41d1
sachu0dev <sushil.dev.in@gmail.com>
2024-02-08 11:05:18 +0530

    biult: keyboard response to key clicks

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,28 @@
+const keys = document.querySelectorAll('.key');
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  function handleKeyPress(event) {
+    const key = event.key.toLowerCase();
+    keys.forEach(function (element) {
+      if (element.textContent.trim().toLowerCase() === key) {
+        element.classList.add('active');
+        setTimeout(()=> {
+          element.classList.remove('active');
+        },200)
+      }
+    });
+  }
+
+  // Event listener for keyboard input
+  document.addEventListener('keypress', handleKeyPress);
+});
+
+
+
+
+
+
 feather.replace();
