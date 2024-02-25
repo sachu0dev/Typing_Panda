e3dd967313e5eb37deefca4ac9c28a86736a7a07
sachu0dev <sushil.dev.in@gmail.com>
2024-02-25 14:34:04 +0530

    insert: inputs

---
+++ scripts/login.js
@@ -0,0 +1,6 @@
+const signUsername = document.getElementById("sign-username");
+const signEmail = document.getElementById("sign-email");
+const signPswd = document.getElementById("sign-pswd");
+
+const loginEmail = document.getElementById("login-email");
+const loginPswd = document.getElementById("login-pswd");
