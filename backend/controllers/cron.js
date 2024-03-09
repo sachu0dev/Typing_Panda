88defc0a5dfb36137dd4f0791f8039c072681613
sachu0dev <sushil.dev.in@gmail.com>
2024-03-09 10:06:38 +0530

    test 11

--- backend/controllers/cron.js
+++ backend/controllers/cron.js
@@ -1,5 +1,5 @@
-const cron = require('node-cron');
 const { UserScore } = require("../modal.js");
+
 async function clearTodaysScore() {
     try {
         await UserScore.updateMany({}, { $set: { todayscore: [] } });
@@ -8,9 +8,5 @@ async function clearTodaysScore() {
         console.error('Error clearing todayscore:', error.message || error);
     }
 }
-cron.schedule("0 0 * *", ()=>{
-    clearTodaysScore();
-})
 
-// Export the function to be deployed as a serverless function
-module.exports = {clearTodaysScore};
+module.exports = { clearTodaysScore };
