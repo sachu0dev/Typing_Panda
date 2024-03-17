f30e4c891bff5f3abcdc823f364cbb3c8e210b01
sachu0dev <sushil.dev.in@gmail.com>
2024-03-17 09:44:19 +0530

    test 5

--- backend/api/cron.js
+++ backend/api/cron.js
@@ -1,12 +1,20 @@
 const cron = require('node-cron');
 const { UserScore } = require("../modal.js");
 
-export default async function handler(request, response) {
+// export default async function cron(request, response) {
+//     try {
+//         await UserScore.updateMany({}, { $set: { todayscore: [] } });
+//         console.log('Todayscore cleared successfully.');
+//     } catch (error) {
+//         console.error('Error clearing todayscore:', error.message || error);
+//     }
+
+// }
+export const cron = async () => {
     try {
         await UserScore.updateMany({}, { $set: { todayscore: [] } });
         console.log('Todayscore cleared successfully.');
     } catch (error) {
         console.error('Error clearing todayscore:', error.message || error);
     }
-
-}
+};
